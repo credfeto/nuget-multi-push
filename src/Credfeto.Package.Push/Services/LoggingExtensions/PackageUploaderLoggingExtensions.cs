4b4e901e120e9efc200dbe841b2528a3aac538f2
Mark Ridgwell <credfeto@users.noreply.github.com>
2023-03-19 00:14:57 +0000

    Merged files

--- src/Credfeto.Package.Push/Services/LoggingExtensions/PackageUploaderLoggingExtensions.cs
+++ src/Credfeto.Package.Push/Services/LoggingExtensions/PackageUploaderLoggingExtensions.cs
@@ -5,6 +5,9 @@ namespace Credfeto.Package.Push.Services.LoggingExtensions;
 
 internal static partial class PackageUploaderLoggingExtensions
 {
-    [LoggerMessage(EventId = 0, Level = LogLevel.Information, Message = "Retrying transient exception {typeName}, on attempt {retryCount} of {maxRetries}. Current delay is {delay}: {details}")]
+    [LoggerMessage(EventId = 0, Level = LogLevel.Error, Message = "Failed to upload {package}: {type} {message}")]
+    public static partial void FailedToUploadPackage(this ILogger<PackageUploader> logger, string package, string type, string message, Exception exception);
+
+    [LoggerMessage(EventId = 1, Level = LogLevel.Information, Message = "Retrying transient exception {typeName}, on attempt {retryCount} of {maxRetries}. Current delay is {delay}: {details}")]
     public static partial void TransientException(this ILogger<PackageUploader> logger, string typeName, int retryCount, int maxRetries, TimeSpan delay, string details, Exception exception);
 }

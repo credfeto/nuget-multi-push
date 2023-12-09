17bdcd61a40f9d7e7945541008babeecbacda925
Mark Ridgwell <credfeto@users.noreply.github.com>
2023-12-09 16:37:51 +0000

    [Cleanup] - Code cleanup on NugetPush.sln

--- src/Credfeto.Package.Push/IUploadOrchestration.cs
+++ src/Credfeto.Package.Push/IUploadOrchestration.cs
@@ -6,5 +6,9 @@ namespace Credfeto.Package.Push;
 
 public interface IUploadOrchestration
 {
-    Task<IReadOnlyList<(string package, bool success)>> PushAllAsync(string source, string? symbolSource, IReadOnlyList<string> packages, string apiKey, CancellationToken cancellationToken);
+    Task<IReadOnlyList<(string package, bool success)>> PushAllAsync(string source,
+                                                                     string? symbolSource,
+                                                                     IReadOnlyList<string> packages,
+                                                                     string apiKey,
+                                                                     CancellationToken cancellationToken);
 }

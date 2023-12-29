7ad5de61a0f8ffe6d24e6e452b4ba630d8d0b596
Mark Ridgwell <credfeto@users.noreply.github.com>
2023-12-29 14:48:59 +0000

    Switching to use Cocona

---
+++ src/Credfeto.Package.Push/Commands.cs
@@ -0,0 +1,97 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Cocona;
+using Credfeto.Package.Push.Exceptions;
+
+namespace Credfeto.Package.Push;
+
+public sealed class Commands
+{
+    private readonly IUploadOrchestration _uploadOrchestration;
+
+    public Commands(IUploadOrchestration uploadOrchestration)
+    {
+        this._uploadOrchestration = uploadOrchestration;
+    }
+
+    [Command(Description = "Uploads all packages in the folder")]
+    [PrimaryCommand]
+    public async Task UploadPackagesAsync([Option(name: "source", ['s'], Description = "NuGet Feed to upload packages to")] string source,
+                                          [Option(name: "folder", ['f'], Description = "Folder containing packages to upload")] string folder,
+                                          [Option(name: "api-key", ['a'], Description = "Api Key for uploading packages")] string apiKey,
+                                          [Option("symbol-source", Description = "NuGet Feed to upload symbol packages to")] string? symbolSource)
+    {
+        IReadOnlyList<string> packages = Searcher.FindMatchingPackages(folder);
+
+        if (packages.Count == 0)
+        {
+            throw new UploadConfigurationErrorsException("folder does not contain any packages");
+        }
+
+        IReadOnlyList<(string package, bool success)> results =
+            await this._uploadOrchestration.PushAllAsync(source: source, symbolSource: symbolSource, packages: packages, apiKey: apiKey, cancellationToken: CancellationToken.None);
+
+        ProduceSummary(results: results);
+    }
+
+    private static void ProduceSummary(IReadOnlyList<(string package, bool success)> results)
+    {
+        OutputPackagesAsAssets(results);
+
+        bool success = OutputUploadSummary(results);
+
+        if (!success)
+        {
+            throw new UploadFailedException();
+        }
+    }
+
+    private static bool OutputUploadSummary(IReadOnlyList<(string package, bool success)> results)
+    {
+        Console.WriteLine("Upload Summary:");
+        int succeeded = 0;
+        int errors = 0;
+
+        foreach ((string package, bool success) in results)
+        {
+            string packageName = Path.GetFileName(package);
+
+            if (success)
+            {
+                Console.WriteLine($"* {packageName} : Uploaded");
+                ++succeeded;
+            }
+            else
+            {
+                Console.WriteLine($"* {packageName} : FAILED");
+                ++errors;
+            }
+        }
+
+        Console.WriteLine($"Total Packages: {results.Count}");
+        Console.WriteLine($"Succeeded: {succeeded}");
+        Console.WriteLine($"Failures: {errors}");
+
+        return errors == 0;
+    }
+
+    private static void OutputPackagesAsAssets(IReadOnlyList<(string package, bool success)> packages)
+    {
+        string? env = Environment.GetEnvironmentVariable("TEAMCITY_VERSION");
+
+        if (string.IsNullOrWhiteSpace(env))
+        {
+            return;
+        }
+
+        foreach (string package in packages.Where(x => x.success)
+                                           .Select(x => x.package))
+        {
+            Console.WriteLine($"##teamcity[publishArtifacts '{package}']");
+        }
+    }
+}
--- src/Credfeto.Package.Push/Configuration/ServiceConfiguration.cs
+++ src/Credfeto.Package.Push/Configuration/ServiceConfiguration.cs
@@ -1,25 +1,15 @@
-using System;
 using Credfeto.Package.Push.Helpers;
 using Credfeto.Package.Push.Services;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
 using ILogger = NuGet.Common.ILogger;
 
 namespace Credfeto.Package.Push.Configuration;
 
 internal static class ServiceConfiguration
 {
-    public static IServiceProvider Configure(bool warningsAsErrors)
-    {
-        DiagnosticLogger logger = new(warningsAsErrors);
-
-        return new ServiceCollection().BuildServiceProvider();
-    }
-
     public static IServiceCollection AddServices(this IServiceCollection services)
     {
         return services.AddSingleton<ILogger, NugetForwardingLogger>()
-                       .AddSingleton(typeof(ILogger<>), typeof(LoggerProxy<>))
                        .AddSingleton<IUploadOrchestration, UploadOrchestration>()
                        .AddSingleton<IPackageUploader, PackageUploader>();
     }

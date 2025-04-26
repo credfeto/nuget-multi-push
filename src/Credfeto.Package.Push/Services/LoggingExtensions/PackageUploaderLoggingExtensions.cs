6194acb0457b0b481d339d07132dde3c183cbb7a
Mark Ridgwell <credfeto@users.noreply.github.com>
2025-04-26 11:33:04 +0000

    Cleanup: PackageUploaderLoggingExtensions.cs

--- src/Credfeto.Package.Push/Services/LoggingExtensions/PackageUploaderLoggingExtensions.cs
+++ src/Credfeto.Package.Push/Services/LoggingExtensions/PackageUploaderLoggingExtensions.cs
@@ -5,11 +5,7 @@ namespace Credfeto.Package.Push.Services.LoggingExtensions;
 
 internal static partial class PackageUploaderLoggingExtensions
 {
-    [LoggerMessage(
-        EventId = 0,
-        Level = LogLevel.Error,
-        Message = "Failed to upload {package}: {type} {message}"
-    )]
+    [LoggerMessage(EventId = 0, Level = LogLevel.Error, Message = "Failed to upload {package}: {type} {message}")]
     public static partial void FailedToUploadPackage(
         this ILogger<PackageUploader> logger,
         string package,

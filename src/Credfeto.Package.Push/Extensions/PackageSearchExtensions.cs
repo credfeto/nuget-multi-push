6c0f66680e672106e4207bdd2fd536a5042779e1
Mark Ridgwell <credfeto@users.noreply.github.com>
2025-05-10 18:56:19 +0000

    Cleanup: PackageSearchExtensions.cs

--- src/Credfeto.Package.Push/Extensions/PackageSearchExtensions.cs
+++ src/Credfeto.Package.Push/Extensions/PackageSearchExtensions.cs
@@ -9,7 +9,11 @@ namespace Credfeto.Package.Push.Extensions;
 
 internal static class PackageSearchExtensions
 {
-    public static string? FindMatchingSymbolPackage(this IReadOnlyList<string> symbolPackages, string package, ILogger logger)
+    public static string? FindMatchingSymbolPackage(
+        this IReadOnlyList<string> symbolPackages,
+        string package,
+        ILogger logger
+    )
     {
         if (symbolPackages.Count == 0)
         {
@@ -23,15 +27,21 @@ internal static class PackageSearchExtensions
         string expectedSymbolOld = baseName + PackageNaming.SymbolsOldPackageExtension;
         string expectedSymbolNew = baseName + PackageNaming.SymbolsNewPackageExtension;
 
-        return symbolPackages.FindMatchingSymbolByFullName(expectedSymbol: expectedSymbolNew, logger: logger) ??
-               symbolPackages.FindMatchingSymbolByFullName(expectedSymbol: expectedSymbolOld, logger: logger);
+        return symbolPackages.FindMatchingSymbolByFullName(expectedSymbol: expectedSymbolNew, logger: logger)
+            ?? symbolPackages.FindMatchingSymbolByFullName(expectedSymbol: expectedSymbolOld, logger: logger);
     }
 
-    private static string? FindMatchingSymbolByFullName(this IReadOnlyList<string> symbolPackages, string expectedSymbol, ILogger logger)
+    private static string? FindMatchingSymbolByFullName(
+        this IReadOnlyList<string> symbolPackages,
+        string expectedSymbol,
+        ILogger logger
+    )
     {
         logger.LookingForSymbolsPackage($"Looking for Symbols Package: {expectedSymbol}");
 
-        string? symbolSource = symbolPackages.FirstOrDefault(x => StringComparer.OrdinalIgnoreCase.Equals(x: x, y: expectedSymbol));
+        string? symbolSource = symbolPackages.FirstOrDefault(x =>
+            StringComparer.OrdinalIgnoreCase.Equals(x: x, y: expectedSymbol)
+        );
 
         if (symbolSource is not null)
         {
